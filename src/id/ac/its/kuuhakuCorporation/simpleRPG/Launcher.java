e32ad06e84867383b5db20a583c703ae290b4e9b
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2019-11-27 12:08:29 +0700

    Making and testing Display class

--- src/id/ac/its/kuuhakuCorporation/simpleRPG/Launcher.java
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/Launcher.java
@@ -1,7 +1,9 @@
 package id.ac.its.kuuhakuCorporation.simpleRPG;
 
+import id.ac.its.kuuhakuCorporation.simpleRPG.display.Display;
+
 public class Launcher {
 	public static void main(String[] args) {
-		
+		new Display("Title!", 300, 300);
 	}
 }
