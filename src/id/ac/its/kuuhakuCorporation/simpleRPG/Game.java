8052b7511ac4c3bb6894a54856b9b8d75a951295
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2019-11-27 17:30:10 +0700

    Update Game.java

--- src/id/ac/its/kuuhakuCorporation/simpleRPG/Game.java
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/Game.java
@@ -2,15 +2,57 @@ package id.ac.its.kuuhakuCorporation.simpleRPG;
 
 import id.ac.its.kuuhakuCorporation.simpleRPG.display.Display;
 
-public class Game {
+public class Game implements Runnable{
 	private Display display;
-	
 	public int width,height;
+	private boolean running = false;
+	
+	private Thread thread;
 	
 	public Game(String title,int height,int width) {
 		this.width = width;
 		this.height = height;
 		
 		display = new Display(title,width,height);
+	}
+	
+	private void init() {
+		
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		
+	}
+	
+	public void run() {
+		 init();
+		 
+		 while(running) {
+			 tick();
+			 render();
+		 }
+		 
+		 stop();
+	}
+	
+	public synchronized void start() {
+		if(running) return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		if(!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
