901224718e6a490d8591f28679425824ce997b68
ilhambayhaqi <ilham.bhq28@gmail.com>
2019-12-03 02:25:14 +0700

    add LoadWorld and Create Map

--- src/id/ac/its/kuuhakuCorporation/simpleRPG/states/GameState.java
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/states/GameState.java
@@ -14,7 +14,7 @@ public class GameState extends State{
 	public GameState(Game game) {
 		super(game);
 		player = new Player(game, 100, 100);
-		world = new World("");
+		world = new World("res/worlds/world1.txt");
 	}
 	
 	@Override
--- src/id/ac/its/kuuhakuCorporation/simpleRPG/worlds/World.java
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/worlds/World.java
@@ -3,10 +3,12 @@ package id.ac.its.kuuhakuCorporation.simpleRPG.worlds;
 import java.awt.Graphics;
 
 import id.ac.its.kuuhakuCorporation.simpleRPG.tiles.Tile;
+import id.ac.its.kuuhakuCorporation.simpleRPG.utils.Utils;
 
 public class World {
 	
 	private int width, height;
+	private int spawnX, spawnY;
 	private int[][] tiles;
 
 	public World(String path) {
@@ -33,6 +35,18 @@ public class World {
 	}
 
 	private void loadWorld(String path) {
-
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		width = Utils.parseInt(tokens[0]);
+		height = Utils.parseInt(tokens[1]);
+		spawnX = Utils.parseInt(tokens[2]);
+		spawnY = Utils.parseInt(tokens[3]);
+		
+		tiles = new int[width][height];
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				tiles[x][y] = Utils.parseInt(tokens[(x + y * width) + 4]);
+			}
+		}
 	}
 }
