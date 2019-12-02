4f5318da9e22b362b23853fda6bee0875267583e
ilhambayhaqi <ilham.bhq28@gmail.com>
2019-12-02 23:01:06 +0700

    Add Creature Sizing and cleaning up move method

--- src/id/ac/its/kuuhakuCorporation/simpleRPG/entities/creatures/Player.java
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/entities/creatures/Player.java
@@ -10,26 +10,34 @@ public class Player extends Creature {
 	private Game game;
 	
 	public Player(Game game, float x, float y) {
-		super(x, y);
+		super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 		this.game = game;
 	}
 
 	@Override
 	public void tick() {
-		if(game.getKeyManager().up)
-			y -= 3;
-		if(game.getKeyManager().down)
-			y += 3;
-		if(game.getKeyManager().left)
-			x -= 3;
-		if(game.getKeyManager().right)
-			x += 3;
+		getInput();
+		move();
 		
+	}
+	
+	private void getInput() {
+		xMove = 0;
+		yMove = 0;
+		
+		if(game.getKeyManager().up)
+			yMove = -speed;
+		if(game.getKeyManager().down)
+			yMove = speed;
+		if(game.getKeyManager().left)
+			xMove = -speed;
+		if(game.getKeyManager().right)
+			xMove = speed;
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player,(int) x,(int) y, null);
+		g.drawImage(Assets.player,(int) x,(int) y, width, height, null);
 	}
 	
 }
