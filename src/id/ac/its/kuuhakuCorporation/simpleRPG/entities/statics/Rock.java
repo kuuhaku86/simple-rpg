7c25c5d5354e7c5960c7106bf85db7f3781ec955
ilhambayhaqi <57692117+ilhambayhaqi@users.noreply.github.com>
2019-12-12 09:25:46 +0700

    Add Rock Static

---
+++ src/id/ac/its/kuuhakuCorporation/simpleRPG/entities/statics/Rock.java
@@ -0,0 +1,42 @@
+package id.ac.its.kuuhakuCorporation.simpleRPG.entities.statics;
+
+import java.awt.Graphics;
+
+import id.ac.its.kuuhakuCorporation.simpleRPG.Handler;
+import id.ac.its.kuuhakuCorporation.simpleRPG.gfx.Assets;
+import id.ac.its.kuuhakuCorporation.simpleRPG.items.Item;
+import id.ac.its.kuuhakuCorporation.simpleRPG.tiles.Tile;
+import id.ac.its.kuuhakuCorporation.simpleRPG.utils.Sound;
+
+public class Rock extends StaticEntity {
+
+	public Rock(Handler handler, float x, float y) {
+		super(handler, x, y, Tile.TILEWIDTH, Tile.TILEHEIGHT);
+		health = 10;
+		bounds.x = 3;
+		bounds.y = (int) (height / 2f);
+		bounds.width = width - 6;
+		bounds.height = (int) (height - height / 1.5f);
+	}
+
+	@Override
+	public void tick() {
+
+	}
+
+	@Override
+	public void die() {
+		Sound.rock.play();
+		drop = (int) (Math.random()*100);
+		if(drop > 7)
+			handler.getWorld().getItemManager().addItem(Item.rockItem.createNew((int) x, (int) y));
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(Assets.rock, (int) (x - handler.getGameCamera().getxOffset()),
+				(int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+
+	}
+
+}
